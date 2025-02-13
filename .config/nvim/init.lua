ea548793520c3ae5b8a9e5ea68e7be37e80c985a
riskirills66 <riskirills66@gmail.com>
2025-02-13 14:18:39 +0700

    nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,7 +50,7 @@ vim.opt.clipboard = "unnamedplus"
 --     }
 -- end
 
-vim.opt.termguicolors = false  -- Disable GUI colors, force terminal colors
+vim.opt.termguicolors = false -- Disable GUI colors, force terminal colors
 vim.cmd([[
   set t_Co=256
   hi Normal ctermbg=NONE ctermfg=NONE
@@ -60,43 +60,49 @@ vim.cmd([[
 ]])
 
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 100,
-      tabline = 100,
-      winbar = 100,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 100,
+			tabline = 100,
+			winbar = 100,
+		}
+	}, sections = {
+	lualine_a = { 'mode' },
+	lualine_b = { 'branch', 'diff', 'diagnostics' },
+	lualine_c = { 'filename' },
+	lualine_x = {
+		'encoding', 'fileformat', 'filetype',
+		{
+			require("noice").api.statusline.mode.get,
+			cond = require("noice").api.statusline.mode.has,
+			color = { fg = "#ff9e64" },
+		}
+	},
+	lualine_y = { 'progress' },
+	lualine_z = { 'location' }
+},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
 }
