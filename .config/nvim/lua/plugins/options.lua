0eb94124a2adde7322e68ef6dc3b73fd068f8a23
riskirills66 <riskirills66@gmail.com>
2025-02-14 12:43:15 +0700

    nvim config fix bufferline tab missing

--- .config/nvim/lua/plugins/options.lua
+++ .config/nvim/lua/plugins/options.lua
@@ -1,3 +1,4 @@
 -- vim.cmd("colorscheme catppuccin-macchiato") -- set color theme
 
-require("bufferline").setup{} --bufferline
+--require("bufferline").setup{} --bufferline
+
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -6,7 +6,34 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons"
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        require("bufferline").setup({
+            options = {
+                mode = "buffers",
+                numbers = "none",
+                close_command = "bdelete! %d",
+                right_mouse_command = "bdelete! %d",
+                left_mouse_command = "buffer %d",
+                middle_mouse_command = nil,
+                indicator = {
+                    icon = '▎',
+                    style = 'icon',
+                },
+                buffer_close_icon = '󰅖',
+                modified_icon = '●',
+                close_icon = '',
+                left_trunc_marker = '',
+                right_trunc_marker = '',
+                show_buffer_icons = true,
+                show_buffer_close_icons = true,
+                show_close_icon = true,
+                show_tab_indicators = true,
+                separator_style = "thin",
+                always_show_bufferline = true,
+            }
+        })
+    end
   },
   {
     "folke/noice.nvim",
