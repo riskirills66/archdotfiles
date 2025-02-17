090726113d39620228b225c12104a0de3240884b
riskirills66 <riskirills66@gmail.com>
2025-02-17 23:43:24 +0700

    nvim config, change format keybind to leader/

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<leader>x", ":bd<cr>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>")
 
 -- comment toggle
-vim.keymap.set({"n", "v"}, "<leader>fmc", ":CommentToggle<cr>")
+vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<cr>")
 
 -- format code using LSP
 vim.keymap.set("n", "<leader>fmd", vim.lsp.buf.format)
